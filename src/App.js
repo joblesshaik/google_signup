13e4e7ec841cba6630e8d100c70052a7453bd158
mohammed sarfaraz <mohammedsarfaraz18@gmail.com>
2022-04-26 14:50:34 +0530

    first commit

--- src/App.js
+++ src/App.js
@@ -1,23 +1,23 @@
-import logo from './logo.svg';
+// import logo from './logo.svg';
 import './App.css';
+import{Auntentication} from "./firebase"
+import {  signInWithPopup, GoogleAuthProvider } from "firebase/auth";
 
 function App() {
+  const SignINWitgGoogle=()=>{
+    const provider = new GoogleAuthProvider();
+    signInWithPopup(Auntentication, provider)
+    .then((re)=>{
+      console.log(re);
+    })
+    .catch((err)=>{
+      console.log(err);
+    })
+
+  }
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+   <button onClick={SignINWitgGoogle}>click me</button>
     </div>
   );
 }
